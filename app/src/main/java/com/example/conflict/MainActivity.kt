891b226c12bd804aad0cc90d0ed7c3968c8190a3
ThaoLT1 <ThaoLT1@example.com>
2025-01-17 11:14:49 +0700

    commit thao

--- app/src/main/java/com/example/conflict/MainActivity.kt
+++ app/src/main/java/com/example/conflict/MainActivity.kt
@@ -13,6 +13,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.conflict.ui.theme.ConflictTheme
 
+class User{
+    val userName = "Thao"
+    val age = 22
+}
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
